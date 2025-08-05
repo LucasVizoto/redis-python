391fc130c27512824fffabad8e8a6e0cf0a164ce
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-08-05 18:42:30 -0300

    feat: implementing redis connection

---
+++ src/models/redis/settings/connection.py
@@ -0,0 +1,22 @@
+from redis import Redis
+from os import getenv
+
+class RedisConnectionHandler:
+    def __init__(self) -> None:
+        self.__redis_conn = None
+
+    def connect(self) -> Redis:
+        redis_conn = Redis(
+            host= getenv("DATABASE_HOST"), 
+            port= getenv("DATABASE_PORT"), 
+            db=0, 
+            password= getenv("DATABASE_PASSWORD"), 
+            username= getenv("DATABASE_USERNAME")
+        )
+        self.__redis_conn = redis_conn
+        return self.__redis_conn
+
+    def get_connection(self) -> Redis:
+        if self.__redis_conn is None:
+            raise ValueError("Redis connection not established. Call connect() first.")
+        return self.__redis_conn
