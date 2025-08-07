e39d007e4aac98670413eebf26f20c56f12319d5
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-08-07 18:13:52 -0300

    feat: implementing SQLite connection handler

---
+++ src/models/sqlite/settings/connection.py
@@ -0,0 +1,17 @@
+from sqlite3 import Connection, connect
+
+class SQLiteConnectionHandler:
+    def __init__(self) -> None:
+        self.__connection_string = "storage.db"
+        self.__conn = None
+    
+    def connect(self) -> Connection:
+        conn = connect(
+            self.__connection_string,
+            check_same_thread = False
+            )
+        self.__conn = conn
+        return conn
+    
+    def get_connection(self) -> Connection:
+        return self.__conn
